6de6f4569be46ea33e3fe9cf98e954a9240c398f
mehmed <mehmed2kw@gmail.com>
2023-05-02 20:55:00 +0300

    updated Forecast component

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 import "./forecast.scss";
 
-const Forecast = () => {
+const weekDaysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = weekDaysList.slice(dayInAWeek, weekDaysList.length).concat(weekDaysList.slice(0, dayInAWeek));
+
   return (
-    <div>Forecast</div>
+    <div className="forecast">
+      <div className="shell">
+        <div className="forecast__inner">
+          <Accordion>
+            {data.map((item, idx) => {
+              return <AccordionItem key={idx}>
+                <AccordionItemHeading>
+                  <AccordionItemButton>
+                    <div className="card-forecast">
+                      <div className="card__left">
+                        <div className="card__day">
+                          <h2>{forecastDays[idx]}</h2>
+                        </div>
+
+                        <div className="card__temperature">
+                          <img src={`/icons/${item.weatherIcon}.png`} alt="" />
+                          <h1>{Math.round(item.maxTemp)}°C</h1>
+                          <p>/{Math.round(item.minTemp)}°C</p>
+                        </div>
+
+                        <div className="card__description">
+                          <h2>{item.description}</h2>
+                        </div>
+                      </div>
+
+                      <div className="card__rain">
+                        <i className="fa-solid fa-droplet fa-lg"></i>
+                        <p>0%</p>
+                      </div>
+                    </div>
+                  </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+
+                </AccordionItemPanel>
+              </AccordionItem>
+            })}
+          </Accordion>
+        </div>
+      </div>
+    </div>
   )
 }
 
