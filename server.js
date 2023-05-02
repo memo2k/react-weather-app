63ad7e9ec2acb0efaa1ce7843f795dcbc6c0f224
mehmed <mehmed2kw@gmail.com>
2023-05-02 20:54:00 +0300

    added api proxy server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const weatherUrl = process.env.REACT_APP_WEATHER_URL;
+const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/weather', async (req, res) => {
+    const apiUrl = `${weatherUrl}/weather?q=${req.query.q}&appid=${weatherApiKey}&units=metric`;
+
+    await axios.get(apiUrl).then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    })
+});
+
+app.get('/forecast', async (req, res) => {
+    const apiUrl = `${weatherUrl}/forecast?q=${req.query.q}&appid=${weatherApiKey}&units=metric`;
+
+    await axios.get(apiUrl).then((response) => {
+        res.json(response.data);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    })
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
