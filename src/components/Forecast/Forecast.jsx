a24724c2f58e6a29477b66a8e0c510070531f5c2
mehmed <mehmed2kw@gmail.com>
2023-05-06 10:24:50 +0300

    updated Forecast component

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import {
-  Accordion,
-  AccordionItem,
-  AccordionItemHeading,
-  AccordionItemButton,
-  AccordionItemPanel,
-} from "react-accessible-accordion";
 import "./forecast.scss";
 
 const weekDaysList = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -18,43 +11,36 @@ const Forecast = ({ data }) => {
     <div className="forecast">
       <div className="shell">
         <div className="forecast__inner">
-          <Accordion>
-            {data.map((item, idx) => {
-              if (item.weatherIcon != null) {
-              return (<AccordionItem key={idx}>
-                <AccordionItemHeading>
-                  <AccordionItemButton>
-                    <div className="card-forecast">
-                      <div className="card__left">
-                        <div className="card__day">
-                          <h2>{forecastDays[idx]}</h2>
-                        </div>
-
-                        <div className="card__temperature">
-                          <img src={`/icons/${item.weatherIcon}.png`} alt="" />
-                          <h1>{Math.round(item.maxTemp)}°C</h1>
-                          <p>/{Math.round(item.minTemp)}°C</p>
-                        </div>
-
-                        <div className="card__description">
-                          <h2>{item.description}</h2>
-                        </div>
-                      </div>
-
-                      <div className="card__rain">
-                        <i className="fa-solid fa-cloud fa-lg"></i>
-                        <p>{item.clouds}%</p>
-                      </div>
+          {data.map((item, idx) => {
+            if (item.weatherIcon != null) {
+              return (
+                <div className="card-forecast" key={idx}>
+                  <div className="card__left">
+                    <div className="card__day">
+                      <h2>{forecastDays[idx]}</h2>
                     </div>
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel>
-                  
-                </AccordionItemPanel>
-              </AccordionItem>
-              )}
-            })}
-          </Accordion>
+
+                    <div className="card__temperature">
+                      <div className="card__image">
+                        <img src={`/icons/${item.weatherIcon}.png`} alt="" />
+                      </div>
+                      <h1>{Math.round(item.maxTemp)}°C</h1>
+                      <p>/{Math.round(item.minTemp)}°C</p>
+                    </div>
+
+                    <div className="card__description">
+                      <h2>{item.description}</h2>
+                    </div>
+                  </div>
+
+                  <div className="card__clouds">
+                    <i className="fa-solid fa-cloud fa-lg"></i>
+                    <p>{item.clouds}%</p>
+                  </div>
+                </div>
+              )
+            }
+          })}
         </div>
       </div>
     </div>
