902edd52de3de5cdd4560971c94fe283a7461b6e
mehmed <mehmed2kw@gmail.com>
2023-05-06 12:57:29 +0300

    updated Forecast component

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -13,35 +13,33 @@ const Forecast = ({ data }) => {
     <div className="forecast">
       <div className="shell">
         <div className="forecast__inner">
-          {data.map((item, idx) => {
-            if (item.weatherIcon != null) {
-              return (
-                <div className="card-forecast" key={idx}>
-                  <div className="card__left">
-                    <div className="card__day">
-                      <h2>{forecastDays[idx]}</h2>
-                    </div>
-
-                    <div className="card__temperature">
-                      <div className="card__image">
-                        <img src={`/icons/${item.weatherIcon}.png`} alt="" />
-                      </div>
-                      <h1>{Math.round(item.maxTemp)}°C</h1>
-                      <p>/{Math.round(item.minTemp)}°C</p>
-                    </div>
-
-                    <div className="card__description">
-                      <h2>{item.description}</h2>
-                    </div>
+          {data.filter(item => item.weatherIcon != null).map((item, idx) => {
+            return (
+              <div className="card-forecast" key={idx}>
+                <div className="card__left">
+                  <div className="card__day">
+                    <h2>{forecastDays[idx]}</h2>
                   </div>
 
-                  <div className="card__clouds">
-                    <i className="fa-solid fa-cloud fa-lg"></i>
-                    <p>{item.clouds}%</p>
+                  <div className="card__temperature">
+                    <div className="card__image">
+                      <img src={`/icons/${item.weatherIcon}.png`} alt="" />
+                    </div>
+                    <h1>{Math.round(item.maxTemp)}°C</h1>
+                    <p>/{Math.round(item.minTemp)}°C</p>
+                  </div>
+
+                  <div className="card__description">
+                    <h2>{item.description}</h2>
                   </div>
                 </div>
-              )
-            }
+
+                <div className="card__clouds">
+                  <i className="fa-solid fa-cloud fa-lg"></i>
+                  <p>{item.clouds}%</p>
+                </div>
+              </div>
+            )
           })}
         </div>
       </div>
